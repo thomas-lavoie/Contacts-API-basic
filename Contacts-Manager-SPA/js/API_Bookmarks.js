aefd2dd3f2db8e01f718044585c7a268d942673b
thomas-lavoie <143450543+thomas-lavoie@users.noreply.github.com>
2024-09-11 00:07:34 -0400

    Update API_Bookmarks.js

--- Contacts-Manager-SPA/js/API_Bookmarks.js
+++ Contacts-Manager-SPA/js/API_Bookmarks.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api/bookmarks";
+const API_URL = "https://aware-scandalous-aristosuchus.glitch.me/api/bookmarks";
 function API_GetBookmarks() {
     return new Promise(resolve => {
         $.ajax({
